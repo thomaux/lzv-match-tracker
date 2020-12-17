b30eb168d590ccae2dfd5a268aacaf41697220d4
Thomas Anciaux <1404536+thomaux@users.noreply.github.com>
2020-12-17 19:51:53 +0100

    Add ServiceWorker support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import { render } from 'react-dom';
 import { App } from './app';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 render(
     <App />,
     document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
